dbd18e0618c78eb13385a15366a1bf7a38b417cc
asbyvshev <barseto4ka@live.ru>
2020-08-05 17:38:50 +0300

    JavaEE. Добавлены простые реализации фильтров.

--- java-ee-shop/src/main/java/ru/geekbrains/filters/NavBarFilter.java
+++ java-ee-shop/src/main/java/ru/geekbrains/filters/NavBarFilter.java
@@ -1,7 +1,27 @@
 package ru.geekbrains.filters;
 
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
-public class NavBarFilter {
+public class NavBarFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterConfig.getServletContext().getRequestDispatcher("/index.jsp").include(servletRequest,servletResponse);
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }

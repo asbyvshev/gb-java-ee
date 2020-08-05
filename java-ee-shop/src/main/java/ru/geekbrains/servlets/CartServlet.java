ed2f7705a4be85139e50e6cdade0bf2ab4d05192
asbyvshev <barseto4ka@live.ru>
2020-08-05 17:37:09 +0300

    JavaEE. Добавлены простые реализации сервлетов.

--- java-ee-shop/src/main/java/ru/geekbrains/servlets/CartServlet.java
+++ java-ee-shop/src/main/java/ru/geekbrains/servlets/CartServlet.java
@@ -1,7 +1,16 @@
 package ru.geekbrains.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name="CartServlet",urlPatterns = "/cart")
-public class CartServlet {
+public class CartServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Some implementation cart!");
+    }
 }
--- java-ee-shop/src/main/java/ru/geekbrains/servlets/MainPageServlet.java
+++ java-ee-shop/src/main/java/ru/geekbrains/servlets/MainPageServlet.java
@@ -1,7 +1,16 @@
 package ru.geekbrains.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "MainPageServlet",urlPatterns = "/main")
-public class MainPageServlet {
+public class MainPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Some implementation main page!");
+    }
 }

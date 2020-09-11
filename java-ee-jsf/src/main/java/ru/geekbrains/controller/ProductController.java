3ae459652e9b5702450d84cd93feb3755d1c78d6
asbyvshev <barseto4ka@live.ru>
2020-09-11 14:57:06 +0300

    JavaEE.JSF.EJB.Удалил лишние импорты.

--- java-ee-jsf/src/main/java/ru/geekbrains/controller/ProductController.java
+++ java-ee-jsf/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -2,17 +2,13 @@ package ru.geekbrains.controller;
 
 import ru.geekbrains.persist.Category;
 import ru.geekbrains.persist.CategoryRepository;
-import ru.geekbrains.persist.Product;
-import ru.geekbrains.persist.ProductRepository;
 import ru.geekbrains.service.ProductRepr;
 import ru.geekbrains.service.ProductService;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.List;
 
 @SessionScoped

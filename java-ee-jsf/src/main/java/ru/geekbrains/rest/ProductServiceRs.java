4728d54698175d24bf12443469ec54258771fc86
asbyvshev <barseto4ka@live.ru>
2020-09-23 09:55:30 +0300

    JavaEE.JSF.EJB.Разделил по разным Url запрос с id и name.

--- java-ee-jsf/src/main/java/ru/geekbrains/rest/ProductServiceRs.java
+++ java-ee-jsf/src/main/java/ru/geekbrains/rest/ProductServiceRs.java
@@ -25,7 +25,7 @@ public interface ProductServiceRs {
     void delete(@PathParam("id") long id);
 
     @GET
-    @Path("/{id}")
+    @Path("/getById/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     ProductRepr findByIdRs(@PathParam("id") long id);
 
@@ -35,7 +35,7 @@ public interface ProductServiceRs {
     List<ProductRepr> findByCategoryId(@PathParam("id") long id);
 
     @GET
-    @Path("/{name}")
+    @Path("/getByName/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     ProductRepr findByName(@PathParam("name") String name);
 
